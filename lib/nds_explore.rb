b4134fb8e36078238ced22a40f04e735a89eecb8
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-22 23:50:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,5 +12,5 @@ def print_first_directors_movie_titles
   while index < directors_database[0][:movies].length do
     print directors_database[0][:movies][index][:title]
     index += 1
-  end 
+  end
 end
